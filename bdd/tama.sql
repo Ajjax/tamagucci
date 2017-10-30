d66a28655d951df6c58758e7e5ef0af255d8ed2e
Ajjax <trilab@hotmail.fr>
2017-10-30 10:04:57 +0100

    bdd
    base de donnéé en ligne

---
+++ bdd/tama.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 30 Octobre 2017 à 09:04
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `tama`
+--
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tam`
+--
+
+CREATE TABLE `tam` (
+  `id` int(11) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `life` int(11) NOT NULL,
+  `miam` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `tam`
+--
+
+INSERT INTO `tam` (`id`, `owner`, `name`, `life`, `miam`) VALUES
+(1, 'hanobenjamin', 'Coco', 150, '2017-10-27 20:23:19');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `mail`, `password`) VALUES
+(1, 'hanobenjamin', 'trilab@hotmail.fr', 'harrogath');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `tam`
+--
+ALTER TABLE `tam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `tam`
+--
+ALTER TABLE `tam`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
